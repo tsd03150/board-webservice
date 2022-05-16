4eec9c478855cea15ac0493ddb2ccdec524f5980
tsd03150 <tsd03150@naver.com>
2022-05-16 16:14:41 +0900

    220516 Initial commit

---
+++ src/main/java/com/kaveloper/portfolio/boardwebservice/BoardWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kaveloper.portfolio.boardwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWebserviceApplication.class, args);
+	}
+
+}
